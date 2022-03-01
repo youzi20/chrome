c61689aa543a2080300dfde505d7e5f5056ca5be
youzi <2361746314@qq.com>
2022-03-01 23:40:53 +0800

    up

--- document/index.js
+++ document/index.js
@@ -1,6 +1,10 @@
 (() => {
     'use strict';
 
+    let page = {
+        x: 0,
+        y: 0
+    };
     let selectDom = null;
     let maskDiv = null;
 
@@ -16,7 +20,7 @@
 
         if (!maskDiv) {
             maskDiv = document.createElement("div");
-            maskDiv.style = "position: fixed; background: rgba(0, 0, 0, 0.5);pointer-events: none;z-index: 9999999;";
+            maskDiv.style = "position: fixed; background: rgba(24, 112, 240, 0.5);pointer-events: none;z-index: 9999999;";
             document.body.appendChild(maskDiv);
         }
 
@@ -26,16 +30,36 @@
         maskDiv.style.left = left + "px";
     }
 
+    const createContextMenu = () => {
+// const div = document
+    }
+
     const onMouseMoveHandle = (e) => {
         const { clientX, clientY } = e;
-        
+
         const element = document.elementFromPoint(clientX, clientY);
+
+        page = { x: clientX, y: clientY };
+
+        if (selectDom && selectDom === element) return;
+
+        selectDom = element;
 
         createMask(element);
     }
 
+    const onClickHandle = (e) => {
+
+    }
+
+    const onContextmenuHandle = (e) => {
+
+    }
+
     const eventListener = () => {
         window.addEventListener("mousemove", onMouseMoveHandle);
+        window.addEventListener("click", onClickHandle);
+        window.addEventListener("contextmenu", onContextmenuHandle)
     }
 
     init();
